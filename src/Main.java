6dc997221dc0e8f9b0a8a7613992b39e6c13160b
Creepyfie <fynjybjyb@mail.ru>
2024-07-04 21:48:59 +0300

    Task Completed

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> myList = List.of(10,2,3,45,1,23,4,23);
+
+        Result<Integer> myResult = findMinMax(myList);
+        System.out.println("Max Value = " + myResult.max());
+        System.out.println("Min Value = " + myResult.min());
+    }
+    public static <T extends Comparable<T>> Result<T> findMinMax(Collection<T> elements) {
+        return findMinMax(elements, Comparable::compareTo);
+    }
+
+    public static <T extends Comparable<T>> Result<T> findMinMax(Collection<T> elements, Comparator<T> comparator) {
+
+        T min = elements.iterator().next();
+        T max = elements.iterator().next();
+        for (T element:
+             elements) {
+            if (comparator.compare(element,min) < 0) {
+                min = element; }
+            else if (comparator.compare(element,max)>0) {
+                max = element;
+            }
+
+        }
+        return new Result<>(min, max);
+    }
+}
+
+record Result<T>(T min, T max) {
+}
