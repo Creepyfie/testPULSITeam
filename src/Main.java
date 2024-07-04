7c1ed4f20e639a6112184bb2da87633fc123fd1d
Creepyfie <fynjybjyb@mail.ru>
2024-07-04 23:26:50 +0300

    Some Refactoring

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> myList = List.of(10d,2d,3d,45d,1d,23d,4d,23d);
+        List<Double> myList = List.of(10d, 2d, 3d, 45d, 1d, 23d, 4d, 23d);
 
         Result<Double> myResult = findMinMax(myList);
         System.out.println("Max Value = " + myResult.max());
         System.out.println("Min Value = " + myResult.min());
     }
+
     public static <T extends Comparable<T>> Result<T> findMinMax(Collection<T> elements) {
         return findMinMax(elements, Comparable::compareTo);
     }
@@ -20,11 +21,11 @@ public class Main {
         T min = elements.iterator().next();
         T max = elements.iterator().next();
 
-        for (T element:
-             elements) {
-            if (comparator.compare(element,min) < 0) {
-                min = element; }
-            else if (comparator.compare(element,max)>0) {
+        for (T element :
+                elements) {
+            if (comparator.compare(element, min) < 0) {
+                min = element;
+            } else if (comparator.compare(element, max) > 0) {
                 max = element;
             }
         }
